592ef73f3f623b28ca1038230603058b4eb095bd
Toby <yutan@me.com>
2026-04-04 23:47:43 +0800

    Initial Commit

---
+++ Controller/ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControllerApp.swift
+//  Controller
+//
+//  Created by Toby Yu on 04/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
